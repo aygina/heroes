6f7e25b8c180013b852bbfeea619b3dcf581eca1
aygina <aygina.v@gmail.com>
2022-12-28 16:47:10 +0900

    ranking and localstorage are done!!

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     let heroes = JSON.parse(json);
-    console.log(heroes);
 
     let heroContent = "";
     for (let hero of heroes) {
@@ -12,16 +11,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
         <div>Суперсилы: ${hero.powers}</div><br>
         <div>Подробнее: ${hero.addInfo}</div><br>
         <div>Ваша оценка:
-        <input type="radio" name="rate" id="rate" value="1">
-        <input type="radio" name="rate" id="rate" value="2">2
-        <input type="radio" name="rate" id="rate" value="3">3
-        <input type="radio" name="rate" id="rate" value="4">4
-        <input type="radio" name="rate" id="rate" value="5">5
-        <input type="radio" name="rate" id="rate" value="6">6
-        <input type="radio" name="rate" id="rate" value="7">7
-        <input type="radio" name="rate" id="rate" value="8">8
-        <input type="radio" name="rate" id="rate" value="9">9
-        <input type="radio" name="rate" id="rate" value="10">10
+        <input type="radio" checked="checked" name="${hero.index}-rate" id="rate" value="1/10">1
+        <input type="radio" name="${hero.index}-rate" id="rate" value="2/10">2
+        <input type="radio" name="${hero.index}-rate" id="rate" value="3/10">3
+        <input type="radio" name="${hero.index}-rate" id="rate" value="4/10">4
+        <input type="radio" name="${hero.index}-rate" id="rate" value="5/10">5
+        <input type="radio" name="${hero.index}-rate" id="rate" value="6/10">6
+        <input type="radio" name="${hero.index}-rate" id="rate" value="7/10">7
+        <input type="radio" name="${hero.index}-rate" id="rate" value="8/10">8
+        <input type="radio" name="${hero.index}-rate" id="rate" value="9/10">9
+        <input type="radio" name="${hero.index}-rate" id="rate" value="10/10">10
 </div>`;
     }
 
@@ -30,11 +29,17 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 
 function saveRank() {
-    let rate = document.querySelector('.rate:checked');
-    console.log(rate);
-    rate = rate == null ? '1' : rate.value;
 
-    if (rate == i)
-    return 
-    
-}
+    ranks = [];
+    let saved = document.getElementById('saved');
+    let heroes = JSON.parse(json);
+
+    for (let hero of heroes) {
+    let rank = document.querySelector(`input[name=${hero.index}-rate]:checked`).value;
+    rankInfo = hero.name + ": " + rank;
+    ranks.push(rankInfo);
+
+    console.log(ranks);
+    localStorage.setItem('ranks', JSON.stringify(ranks));
+    saved.innerHTML = 'Сохранено!';
+    }}
